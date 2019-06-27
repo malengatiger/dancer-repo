ed7612bf0e3e20e50236b195dbacdd78a4652792
Aubrey Malabie <malengatiger@gmail.com>
2019-06-27 18:00:19 +0200

    removed old api

--- ar-data-api/src/server.ts
+++
@@ -1,8 +0,0 @@
-import AftaRobotApp from "./app";
-
-console.log(
-  `\n\n☘️ ☘️  server.ts: 🌽 🌽 🌽   Bootstrapping new AftaRobotApp  .... 🌽 🌽 🌽 `
-);
-const app = new AftaRobotApp();
-
-app.listen();
