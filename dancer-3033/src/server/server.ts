e3b691622be97fd34bda21727dddd8865a0a2b7f
Aubrey Malabie <malengatiger@gmail.com>
2019-07-01 16:56:01 +0200

    🧡💛  removed config info - 🧡💛 security

--- dancer-3033/src/server/server.ts
+++ dancer-3033/src/server/server.ts
@@ -8,29 +8,6 @@ import { Request, Response, NextFunction, Application } from "express";
 import bodyParser from "body-parser";
 import AftaRobotApp from "../app";
 import http from "http";
-/*
-BUILD AND DEPLOY VIA CLOUD RUN
-gcloud builds submit --tag gcr.io/dancer-3303/arapi
-gcloud beta run deploy --image gcr.io/dancer-3303/arapi --platform-managed
-
-https://arapi-7amgwbyxjq-uc.a.run.app/getAssociations
-1-929-270-4297
- PIN
-918412   Get new PIN
-Project ID & number
-dancer-3303 (319923451575)
-
-Phone number
-1-929-270-4297 (standard international rates apply)
-
-////////// ibm cloud
-ibmcloud dev deploy
-
-// update app
-ibmcloud cf push
-https://dancer-3033.eu-gb.cf.appdomain.cloud/ping
-
-*/
 
 export const app: Application = express();
 const server = http.createServer(app);
@@ -52,7 +29,7 @@ server.listen(port, () => {
     `\n🔵🔵🔵  DancerWebAPI started and listening on 🧡 💛  http://localhost:${port} 🧡 💛`,
   );
   console.info(
-    `💕 💕 💕 💕  DancerWepAPI running at: 🧡 💛  ${new Date().toISOString() +
+    `💕 💕 💕 💕  DancerWepAPI running at: 🧡💛  ${new Date().toISOString() +
       "  🙄 🙄 🙄"}`,
   );
 });
