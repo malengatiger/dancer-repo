263e9e7a2ecd6454bc5b604d899ca67091e966d0
Aubrey Malabie <aubrey@aftarobot.com>
2021-08-03 11:31:18 +0200

    refactored controllers to allow sync iii

---
+++ dancer-3033-v2/src/models/settings.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const SettingsModelSchema = new mongoose.Schema(
+    {
+        geofenceRadius: {type: Number, required: true},
+        commuterGeofenceRadius: {type: Number, required: true},
+        vehicleSearchMinutes: {type: Number, required: true},
+        heartbeatIntervalSeconds: {type: Number, required: true},
+        commuterSearchMinutes: {type: Number, required: false},
+        commuterGeoQueryRadius: {type: Number, required: false},
+        numberOfLandmarksToScan: {type: Number, required: false},
+        distanceFilter: {type: Number, required: false},
+        vehicleGeoQueryRadius: {type: Number, required: true},
+        associationId: {type: String, required: true}, 
+        created: {type: String, required: true, default: new Date().toISOString()},
+
+    }
+);
+
+SettingsModelSchema.indexes().push({associationID: 1}, {unique: false});
+SettingsModelSchema.indexes().push({created: 1}, {unique: false});
+
+const SettingsModel = mongoose.model('SettingsModel', SettingsModelSchema);
+export default SettingsModel
