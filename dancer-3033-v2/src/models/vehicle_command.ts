545cda602c13b7555c1d30ecdd2f0e6bf299721f
Aubrey Malabie <aubrey@aftarobot.com>
2021-09-10 20:49:55 +0200

    added remote control models and apis ii

---
+++ dancer-3033-v2/src/models/vehicle_command.ts
@@ -0,0 +1,29 @@
+/*
+class VehicleCommand {
+  String? vehicleID, vehicleCommandID;
+  String? vehicleReg;
+  String? created, command;
+  String? userID;
+  String? responseTopic;
+  String? intervalInSeconds;
+*/
+import mongoose from 'mongoose';
+
+const VehicleCommandSchema = new mongoose.Schema(
+    {
+        vehicleCommandID: {type: String, required: true},
+        vehicleID: {type: String, required: true},
+        vehicleReg: {type: String, required: true},
+        intervalInSeconds: {type: String, required: true},
+        responseTopic: {type: String, required: true},
+        userID: {type: String, required: false},
+        created: {type: String, required: true, default: new Date().toISOString()},
+
+    }
+);
+
+VehicleCommandSchema.indexes().push({vehicleID: 1}, {unique: false});
+VehicleCommandSchema.indexes().push({created: 1}, {unique: false});
+
+const VehicleCommand = mongoose.model('VehicleCommand', VehicleCommandSchema);
+export default VehicleCommand
---
+++ dancer-3033-v2/src/models/vehicle_command_response.ts
@@ -0,0 +1,33 @@
+/*
+class VehicleCommandResponse {
+  String? vehicleCommandResponseID;
+  String? created;
+  bool? commandSucceeded;
+  VehicleCommand? vehicleCommand;
+  VehicleLocation? vehicleLocation;
+  RouteDistanceEstimation? routeDistanceEstimation;
+  String? type;
+*/
+import mongoose from "mongoose";
+
+const VehicleCommandResponseSchema = new mongoose.Schema({
+  vehicleCommandResponseID: { type: String, required: true },
+  commandSucceeded: { type: String, required: true, default: false },
+  vehicleCommand: { type: Map, required: true, default: {}  },
+  vehicleLocation: { type: Map, required: true, default: {}  },
+  routeDistanceEstimation: { type: Map, required: true, default: {} },
+  created: { type: String, required: true, default: new Date().toISOString() },
+});
+
+VehicleCommandResponseSchema.indexes().push(
+  { vehicleID: 1 },
+  { unique: false }
+);
+VehicleCommandResponseSchema.indexes().push({ created: 1 }, { unique: false });
+VehicleCommandResponseSchema.indexes().push({ 'vehicleCommand.vehicleID': 1 }, { unique: false });
+
+const VehicleCommandResponse = mongoose.model(
+  "VehicleCommandResponse",
+  VehicleCommandResponseSchema
+);
+export default VehicleCommandResponse;
