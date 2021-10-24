3ffb432ccddb729b00400cb7221ab20f5645eee8
Aubrey Malabie <aubrey@aftarobot.com>
2021-10-24 11:51:44 +0200

    added ticket types ii

--- dancer-3033-v2/src/models/ticket.ts
+++ dancer-3033-v2/src/models/ticket.ts
@@ -5,13 +5,9 @@ const TicketSchema = new mongoose.Schema(
 
         ticketID: {type: String, required: true, trim: true, index: true},
         ticketTypeAndPrice: {type: Map, required: true},
-        price: {type: Number, required: true},
         numberOfRiders: {type: Number, required: true,default: 1, trim: true},
         user: {type: Map, required: true},
-        routeID: {type: String, required: true},
-        routeName: {type: String, required: true},
-        associationID: {type: String, required: true},
-        associationName: {type: String, required: true},
+       
         startDate: {type: String, required: true},
         endDate: {type: String, required: true},
         isActive: {type: Boolean, required: true, default: false},
@@ -20,10 +16,10 @@ const TicketSchema = new mongoose.Schema(
     }
 );
 
-TicketSchema.index({associationID: 1})
+TicketSchema.index({'ticketTypeAndPrice.associationID': 1})
 TicketSchema.index({startDate: 1})
 TicketSchema.index({endDate: 1})
-TicketSchema.index({routeID: 1})
+TicketSchema.index({'ticketTypeAndPrice.routeID': 1})
 TicketSchema.index({'user.userID': 1})
 const Ticket = mongoose.model('Ticket', TicketSchema);
 export default Ticket
